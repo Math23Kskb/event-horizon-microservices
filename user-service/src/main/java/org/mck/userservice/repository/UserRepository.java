e6aab4b4a8dd29cadc1b446776c462bb2d31e5b2
mathc <mathchiyuki@gmail.com>
2025-06-28 11:49:51 -0300

    feat(user-service): user repository

---
+++ user-service/src/main/java/org/mck/userservice/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package org.mck.userservice.repository;
+
+import org.mck.userservice.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
+}
