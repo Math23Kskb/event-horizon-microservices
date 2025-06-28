adc63d679e447a9bbf78c7ced99eec6d9434ec3e
mathc <mathchiyuki@gmail.com>
2025-06-28 11:50:09 -0300

    feat(user-service): user service

---
+++ user-service/src/main/java/org/mck/userservice/service/UserService.java
@@ -0,0 +1,28 @@
+package org.mck.userservice.service;
+
+import lombok.RequiredArgsConstructor;
+import org.mck.userservice.domain.User;
+import org.mck.userservice.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public List<User> findAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public Optional<User> findUserById(Long id) {
+        return userRepository.findById(id);
+    }
+}
