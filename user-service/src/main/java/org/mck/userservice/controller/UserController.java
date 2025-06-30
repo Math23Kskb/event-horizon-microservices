7b25e8e7bfe28225611d653f1458509aeda26d73
mathc <mathchiyuki@gmail.com>
2025-06-30 14:36:52 -0300

    feat(infra): implement service discovery with Eureka and API Gateway

--- user-service/src/main/java/org/mck/userservice/controller/UserController.java
+++ user-service/src/main/java/org/mck/userservice/controller/UserController.java
@@ -10,24 +10,24 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/v1/users")
+@RequestMapping("api/v1")
 @RequiredArgsConstructor
 public class UserController {
 
     private final UserService userService;
 
-    @PostMapping
+    @PostMapping("/users")
     ResponseEntity<User> createUser(@RequestBody User newUser){
         return ResponseEntity.ok(userService.createUser(newUser));
     }
 
-    @GetMapping
+    @GetMapping("/users")
     ResponseEntity<List<User>> getUser(){
         List<User> users = userService.findAllUsers();
         return ResponseEntity.ok(users);
     }
 
-    @GetMapping("/{id}")
+    @GetMapping("/users/{id}")
     ResponseEntity<User> getUserById(@PathVariable Long id){
         return userService.findUserById(id)
                 .map(ResponseEntity::ok)
